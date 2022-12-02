93cdddd20e27652dd794a5011c256c1f525a08bc
Ruqouyyah Muhammad <99969515+mruqqie@users.noreply.github.com>
2022-12-02 13:54:54 -0800

    modified:   website/app.js 	modified:   website/index.html

--- website/app.js
+++ website/app.js
@@ -1,10 +1,10 @@
 /* Global Variables */
 const apiKey = '&appid=62d0d0939da5822ecf43caf7656b0ffc&units=imperial';
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', pgWeather);
@@ -13,13 +13,31 @@ document.getElementById('generate').addEventListener('click', pgWeather);
 function pgWeather() {
     const zip = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
-    gWeatherData(baseUrl, zip, apiKey)
-    .then((data) => {
-        postData('/addData', {temp: data.main.temp, date: newDate, response: feelings});
-    })
-    .then(
-        updateUI()
-    )
+    const entryHolder = document.getElementById('entryHolder');
+    const zipValid = document.getElementById('zipValid');
+    if (zip.length < 5 || zip.length > 10) {
+        const invalidDiv = document.createElement('div');
+        const invalid = document.createTextNode('Invalid Zip code');
+        invalidDiv.setAttribute('id', 'invalid');
+        invalidDiv.appendChild(invalid);
+        entryHolder.appendChild(invalidDiv);
+        if (zipValid) {
+            zipValid.style.display = 'none';
+        };
+    } else {
+        zipValid.style.display = 'block';
+        gWeatherData(baseUrl, zip, apiKey)
+        .then((data) => {
+            postData('/addData', {temp: data.main.temp, date: newDate, response: feelings});
+        })
+        .then(() => {
+            updateUI()
+            const invalidNode = document.getElementById('invalid');
+            if (invalidNode) {
+                entryHolder.removeChild(invalidNode)
+            };
+        })
+    };
 };
 
 /* Function to GET Web API Data */
